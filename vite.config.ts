b3bbbfce69d428e1092d99a67e36b46b71ccb4f8
neloduka-sobe <nelodukasobe@gmail.com>
2025-03-01 19:38:32 +0100

    Add changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/StaraPlebania/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
