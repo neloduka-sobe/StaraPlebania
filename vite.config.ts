7788ad3e9aa4ba46939dbbf597b1613afa6aa56e
neloduka-sobe <nelodukasobe@gmail.com>
2025-03-02 21:09:31 +0100

    Add optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), compression({ algorithm: "brotliCompress" })],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     minify: "esbuild",
+    treeshake: true,
   },
 });
