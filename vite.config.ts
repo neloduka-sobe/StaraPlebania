cae07c1d669feb6146a0b4e2233f19ab7c49489d
neloduka-sobe <nelodukasobe@gmail.com>
2025-03-30 15:49:15 +0200

    Server side rendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import compression from "vite-plugin-compression";
+import { ssr } from 'vite-plugin-ssr/plugin';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), compression({ algorithm: "brotliCompress" })],
+  plugins: [
+    react(), 
+    compression({ algorithm: "brotliCompress" }), 
+    ssr({
+      prerender: true
+    })
+  ],
   optimizeDeps: {
     include: ['framer-motion', 'react-i18next'],
     exclude: ['lucide-react'],
@@ -13,5 +21,10 @@ export default defineConfig({
   build: {
     minify: "esbuild",
     treeshake: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
 });
